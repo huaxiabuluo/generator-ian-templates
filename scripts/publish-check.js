346bb1f822e170527806678b339d97faf5ddd577
Bruce <huaxiabuluo@hotmail.com>
2023-06-20 20:04:28 +0800

    mod: publish script

--- scripts/publish-check.js
+++ scripts/publish-check.js
@@ -6,6 +6,12 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const ExitCode = {
+  Skip: 0,
+  Normal: 1,
+  Beta: 2,
+};
+
 // const iii = execSync(`git diff HEAD^ HEAD -- package.json | grep '"version":'`, { encoding: 'utf-8' });
 const versionChanged = !!+execSync(
   `git diff -U0 HEAD^ HEAD -- package.json | grep '"version":' > /dev/null && echo 1 || echo 0`,
@@ -14,11 +20,11 @@ const versionChanged = !!+execSync(
 
 if (!versionChanged) {
   // exit with 0 to skip publish
-  process.exit(0);
+  process.exit(ExitCode.Skip);
 }
 
 const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'));
 
 const isBeta = pkg.version.includes('beta');
 
-process.exit(isBeta ? 2 : 1);
+process.exit(isBeta ? ExitCode.Beta : ExitCode.Normal);
