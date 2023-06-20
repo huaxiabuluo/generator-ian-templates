0ac5e41306ddef1dc35397989e315693fd766c2c
Bruce <huaxiabuluo@hotmail.com>
2023-06-20 14:13:27 +0800

    refect: npm-package

--- generators/app/templates/npm-package/babelrc.js
+++ generators/app/templates/npm-package/babelrc.js
@@ -1,5 +1,6 @@
 const { BABEL_ENV } = process.env;
 const cjs = BABEL_ENV === 'commonjs';
+const runtimeVersion = require(`./package.json`).dependencies['@babel/runtime'] || '^7.10.4';
 const loose = true;
 
 module.exports = {
@@ -8,18 +9,19 @@ module.exports = {
       '@babel/preset-env',
       {
         modules: false,
+        useBuiltIns: 'entry',
         corejs: {
           version: 3,
           proposals: true,
         },
       },
     ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
   ],
   plugins: [
     ['@babel/proposal-object-rest-spread', { loose }],
     cjs && ['@babel/transform-modules-commonjs', { loose }],
-    ['@babel/transform-runtime', { useESModules: !cjs }],
+    ['@babel/plugin-transform-runtime', { useESModules: !cjs, version: runtimeVersion }],
   ].filter(Boolean),
 };
--- generators/app/templates/npm-package/rollup.config.js
+++ generators/app/templates/npm-package/rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import replace from '@rollup/plugin-replace';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const noDeclarationFiles = { compilerOptions: { declaration: false } };
 
